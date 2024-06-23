41e6865b8010afa124b2969c19162ebde0082708
Tushar Nagar <tusharrock146@gmail.com>
2024-06-23 19:25:18 +0530

    Update Sidebar.vue

--- client/src/components/Sidebar.vue
+++ client/src/components/Sidebar.vue
@@ -1,36 +1,43 @@
 <template>
-    <div class="sidebar min-h-screen flex flex-col justify-between bg-gray-900 p-2.5">
+    <div class="sidebar bg-gray-800">
         <div class="mt-3.5">
             <img
                 :src={menuIcon}
                 class="ml-2.5 cursor-pointer hover:bg-gray-500"
             />
-8
-            <div class="mt-4 flex items-center gap-2 px-4 py-2 hover:bg-slate-800 rounded-full text-sm text-gray-500 cursor-pointer">
+            <div class="mt-5 flex pt-10 items-center gap-2 px-4 py-2 hover:bg-slate-800 rounded-full text-sm text-gray-500 cursor-pointer">
                 <img :src={newChatIcon} alt="new-chat" class="w-4 h-4" />
                 <p>New Chat</p>
             </div>
+            <div className="recent flex flex-col animate-fadeIn">
+                <p className="recent-title pt-10 mt-30 text-gray-500">Recent</p>
+                <div className="recent-entry flex items-start gap-10 p-3 pr-20 rounded-full text-gray-800 cursor-pointer hover:bg-gray-500">
+                    <img :src={message_icon} alt="" className="h-6 w-6" />
+                    <p class="text-gray-500">Chat</p>
+                </div>
+            </div>
         </div>
 
-        <div class="fixed bottom-0 justify-around items-center gap-2 py-3 px-2">
-            <div class="flex text-gray-500">
-                <img :src={question_icon} alt="" class="w-6 h-6" />
-                <p class="ml-2">Help</p>
+        <div className="bottom flex flex-col">
+            <div className="bottom-item flex items-center gap-2 p-2 cursor-pointer">
+                <img :src={questionIcon} alt="" className="h-6 w-6" />
+                <p class="text-gray-500">Help</p>
             </div>
-            <div class="flex text-gray-500">
-                <img :src={history_icon} alt="" class="w-6 h-6" />
-                <p class="ml-2">Activity</p>
+            <div className="bottom-item flex items-center gap-2 p-2 cursor-pointer">
+                <img :src={historyIcon} alt="" className="h-6 w-6" />
+                <p class="text-gray-500">Activity</p>
             </div>
-            <div class="flex text-gray-500">
-                <img :src={setting_icon} alt="" class="w-6 h-6" />
-                <p class="ml-2">Settings</p>
+            <div className="bottom-item flex items-center gap-2 p-2 cursor-pointer">
+                <img :src={settingsIcon} alt="" className="h-6 w-6" />
+                <p class="text-gray-500">Settings</p>
             </div>
         </div>
+        
     </div>
 </template>
 
 <script>
-import { menuIcon, newChatIcon, settingsIcon, historyIcon, questionIcon } from "../assets"
+import { menuIcon, newChatIcon, settingsIcon, historyIcon, questionIcon, messageIcon } from "../assets"
 import { reactive, ref } from 'vue';
 
 export default {
@@ -41,13 +48,22 @@ export default {
             settingsIcon: settingsIcon,
             historyIcon: historyIcon,
             questionIcon: questionIcon,
+            messageIcon: messageIcon,
         };
     }
 };
 </script>
 
 <style>
-@media (max-width:600px){
+.sidebar {
+	min-height: 100vh;
+	display: inline-flex;
+	flex-direction: column;
+	justify-content: space-between;
+	padding: 10px 15px;
+   
+}
+@media (max-width:450px){
     .sidebar{
         display: none;
     }
