3391f24275a34d5bc31178de37fcbd3e054ccde8
Tushar Nagar <tusharrock146@gmail.com>
2024-06-23 14:42:28 +0530

    Update Sidebar.vue

--- client/src/components/Sidebar.vue
+++ client/src/components/Sidebar.vue
@@ -1,11 +1,24 @@
 <template>
-    <div>
-
+    <div class="min-h-screen flex flex-col justify-between bg-gray-700 p-2.5">
+        <div class="mt-3.5 top">
+            <img
+                :src={menuIcon}
+                class="ml-2.5 cursor-pointer"
+            />
+        </div>
     </div>
 </template>
 
-<script setup>
+<script>
+import { menuIcon } from "../assets/index"
 
+export default {
+    data() {
+        return {
+            menuIcon: menuIcon
+        };
+    }
+};
 </script>
 
 <style lang="scss" scoped>
