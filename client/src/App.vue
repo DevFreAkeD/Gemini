2043542166b6ef5a420e8337439a7c8dc30db3a8
Tushar Nagar <tusharrock146@gmail.com>
2024-06-23 13:46:03 +0530

    Initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app-container">
+    Hello World
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style>
+.app-container {
+  @apply bg-gray-100 min-h-screen flex flex-col;
+}
+</style>
