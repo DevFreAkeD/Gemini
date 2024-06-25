0e7d81e7c09e61b22aaae7e8d48e1f220d7a3d88
Tushar Nagar <tusharrock146@gmail.com>
2024-06-25 10:53:10 +0530

    Update App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="bg-gray-200 min-h-screen flex flex-row">
     <Sidebar />
+    <Main />
   </div>
 </template>
 
 <script>
 import Sidebar from './components/Sidebar.vue';
+import Main from './components/Main.vue';
 
 export default {
   components: {
     Sidebar,
+    Main,
   }
 }
 </script>
