5bc702409331a96ac47d8c789d2f30cbb7218df4
Tushar Nagar <tusharrock146@gmail.com>
2024-06-25 10:53:08 +0530

    Update Main.vue

--- client/src/components/Main.vue
+++ client/src/components/Main.vue
@@ -1,13 +1,34 @@
 <template>
-    <div>
-
+  <div class="bg-gray-950 flex-1 min-h-screen pb-[15vh] relative">
+    <div class="flex items-center justify-between text-[22px] p-5 text-gray-300">
+      <p>Gemini</p>
+      <img src="" alt="" class="w-10 rounded-full" />
     </div>
-</template>
+    <div class="max-w-[900px] mx-auto">
+      <div class="my-12 text-[56px] text-[#c4c7c5] font-medium">
+        <p>
+          <span class="bg-gradient-to-r from-[#4b90ff] to-[#ff5546] bg-clip-text text-transparent">Hello , Developer. </span>
+        </p>
+        <p class="text-gray-300">How Can I Help You Today?</p>
+      </div>
+    </div>
+  </div>
+</template>           
 
-<script setup>
-
+<script>
+  export default {
+    setup() {
+    }
+};
 </script>
 
-<style lang="scss" scoped>
-
+<style>
+@keyframes loader {
+  0% {
+    background-position: -800px 0px;
+  }
+  100% {
+    background-position: 800px 0px;
+  }
+}
 </style>
