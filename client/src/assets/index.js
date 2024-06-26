db11f80a8c6ad727d32270deb099ec6649968bc4
Tushar Nagar <tusharrock146@gmail.com>
2024-06-26 12:48:40 +0530

    Update index.js

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -4,6 +4,11 @@ import questionIcon from "../assets/question_icon.png"
 import historyIcon from "../assets/history_icon.png"
 import messageIcon from "../assets/message_icon.png"
 import settingsIcon from "../assets/setting_icon.png"
+import sendIcon from "../assets/send_icon.png"
+import bulbIcon from "../assets/bulb_icon.png"
+import micIcon from "../assets/mic_icon.png"
+import codeIcon from "../assets/code_icon.png"
+import imgIcon from "../assets/gallary_icon.png"
 
 export {
 	menuIcon,
@@ -12,4 +17,9 @@ export {
 	historyIcon,
 	settingsIcon,
 	messageIcon,
+	sendIcon,
+	bulbIcon,
+	micIcon,
+	codeIcon,
+	imgIcon,
 };
