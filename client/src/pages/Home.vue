1cad7997957e83f06b14f9870abc2f5358c04c14
Tushar Nagar <tusharrock146@gmail.com>
2024-06-26 19:03:53 +0530

    Update Home.vue

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -10,7 +10,7 @@
                 </svg>
             </a>
         </div>
-        <div class="bg-zinc-950 flex flex-col items-center justify-center w-full px-52 py-32">
+        <div class="bg-zinc-950 flex flex-col items-center justify-center w-full px-52 py-32 border-b-[4px] border-zinc-700">
             <h1 class="text-[60px] text-white font-medium">
                 <span class="bg-gradient-to-r from-[#4b90ff] to-[#ff5546] bg-clip-text text-transparent">Gemini</span>
             </h1>
@@ -18,6 +18,15 @@
                 <p class="text-zinc-600 text-[26px]">Your one-stop shop for information. Ask in-depth questions, get creative writing assistance, or bridge language barriers – all powered by cutting-edge AI.</p>
                 <br />
                 <p class="text-zinc-600 text-[26px]">Unleash your potential and explore a world of knowledge. Let's get started!</p>
+            </div>
+            <div class="py-8 items-center justify-center">
+                <a class="bg-gradient-to-r from-[#4b90ff] to-[#ff5546] text-zinc-800 font-medium text-md w-40 px-5 py-2.5 text-center rounded-full text-nowrap min-h-button flex items-center justify-center gap-[0.3em]">
+                    Try Gemini
+                    <svg width="0.625rem" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 9L9 1M9 1H2.5M9 1V7.22222" stroke="currentColor" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"></path>
+                      </svg>
+                      <span className="sr-only">(opens in a new window)</span>
+                </a>
             </div>
         </div>
     </div>
