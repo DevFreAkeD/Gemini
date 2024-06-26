acfe749053711a475919ecfc54b1ac2b90f6683c
Tushar Nagar <tusharrock146@gmail.com>
2024-06-26 18:56:01 +0530

    Create Home.vue

---
+++ client/src/pages/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="bg-zinc-900 w-full">
+        <div class="flex items-center justify-between text-[22px] p-5 border-b-[1px] border-zinc-700">
+            <p class="h1 text-[22px] text-gray-300 pl-6 font-medium">Gemini</p>
+
+            <a class="bg-gradient-to-r from-[#4b90ff] to-[#ff5546] text-zinc-900 font-medium text-sm w-36 px-5 py-2.5 text-center rounded-full text-nowrap min-h-button flex items-center justify-center gap-[0.3em]">
+                Sign In
+                <svg width="0.625rem" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M1 9L9 1M9 1H2.5M9 1V7.22222" stroke="currentColor" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"></path>
+                </svg>
+            </a>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    setup() {
+    }
+};
+</script>
+
+<style>
+
+</style>
