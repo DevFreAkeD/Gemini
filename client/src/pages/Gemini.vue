a06bcc2280915be52ec6286476b0613d053c1481
Tushar Nagar <tusharrock146@gmail.com>
2024-06-27 12:10:55 +0530

    Create Gemini.vue

---
+++ client/src/pages/Gemini.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="bg-gray-200 min-h-screen w-full flex">
+        <Sidebar />
+        <Main />
+    </div>
+</template>
+
+<script>
+import Sidebar from '../components/Sidebar.vue';
+import Main from '../components/Main.vue';
+
+export default {
+  components: {
+    Sidebar,
+    Main,
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
