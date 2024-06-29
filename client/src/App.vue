ddd7691d5a8b12df12fff1ac99ff1b5e3a17ed7d
Tushar Nagar <tusharrock146@gmail.com>
2024-06-29 14:04:35 +0530

    Update App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="bg-gray-200 min-h-screen flex flex-row">
-    <Gemini />
+    <router-view />
   </div>
 </template>
 
 <script>
-import Home from './pages/Home.vue';
-import Gemini from './pages/Gemini.vue'
-
 export default {
   components: {
-    Home,
-    Gemini,
-  }
+  },
 }
 </script>
 
