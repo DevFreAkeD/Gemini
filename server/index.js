8f108600db2f2d6bc7414493f75027317b6c2f0a
Tushar Nagar <tusharrock146@gmail.com>
2024-07-01 15:34:08 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import cors from 'cors';
+import * as dotenv from 'dotenv';
+import connectDB from './mongodb/connect.js';
+import authRoutes from './routes/auth.js';
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json());
+
+app.use('/auth', authRoutes);
 
 app.get('/', async (req, res) => {
     res.send('Hello from Gemini!');
@@ -11,6 +18,7 @@ app.get('/', async (req, res) => {
   
   const startServer = async () => {
     try {
+      await connectDB(process.env.MONGODB_URL); // Ensure the database connection is awaited
       app.listen(8080, () => console.log('Server started on port 8080 http://localhost:8080'));
     } catch (error) {
       console.log(error);
