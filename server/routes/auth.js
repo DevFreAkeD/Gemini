5a5801b618cc8992a5847e6ae1ff952995d8a164
Tushar Nagar <tusharrock146@gmail.com>
2024-07-01 16:12:59 +0530

    Create auth.js

---
+++ server/routes/auth.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import User from '../mongodb/models/User.js';
+
+const router = express.Router();
+const jwtSecret = process.env.JWT_SECRET;
+
+router.post('/register', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = new User({ email, password });
+    await user.save();
+    res.status(201).send('User Registered!');
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(400).send('Invalid credentials!');
+    }
+
+    const token = jwt.sign({ id: user._id }, jwtSecret, { expiresIn: '1h' });
+    res.json({ message: 'Login Successful!', token });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+export default router;
