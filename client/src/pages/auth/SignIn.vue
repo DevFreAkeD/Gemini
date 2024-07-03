261a00abb249963c051a97bef88f290eba167098
Tushar Nagar <tusharrock146@gmail.com>
2024-07-03 20:42:27 +0530

    Update SignIn.vue
    "Updated SignIn and SignUp pages with layout and styling changes, added links to switch between login and register forms"

--- client/src/pages/auth/SignIn.vue
+++ client/src/pages/auth/SignIn.vue
@@ -3,7 +3,7 @@
         <div class="flex items-center justify-between text-[22px] p-5 border-b-[1px] border-zinc-700">
             <p class="h1 text-[22px] text-gray-300 pl-6 font-medium">Gemini</p>
         </div>
-        <div class="bg-zinc-950 flex flex-col items-center justify-center w-full py-28 gap-5 border-b-[4px] border-zinc-700">
+        <div class="bg-zinc-950 flex flex-col items-center justify-center w-full py-24 gap-5 border-b-[4px] border-zinc-700">
             <h1 class="text-[60px] text-white font-medium">
                 <span class="bg-gradient-to-r from-[#4b90ff] to-[#ff5546] bg-clip-text text-transparent">Gemini</span>
             </h1>
@@ -14,6 +14,11 @@
                     <input v-model="password" type="password" placeholder="Password" class="w-full mb-4 p-4 text-zinc-800 rounded-md"/>
                     <button type="submit" class="bg-gradient-to-r from-[#4b90ff] to-[#ff5546] text-zinc-900 font-medium text-sm w-36 px-5 py-2.5 text-center rounded-full text-nowrap min-h-button flex items-center justify-center">Login</button>
                 </form>
+                <p class="text-white pt-2 text-[16px]">Not Registered?
+                   <router-link to="/auth/register" class="underline">
+                      Sign Up
+                   </router-link>
+                </p>
             </div>
         </div>
     </div>
